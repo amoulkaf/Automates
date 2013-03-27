c7fd8986f87516b0e6156e5ed1f6713b56ac3ead
Anouar <anouar@anouar-MS-16Y1.(none)>
2013-03-27 16:13:08 +0100

    Determinisation ok

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-1import automaton
-
+import automaton
+# coding=utf-8
 
 def automate():
     
     alphabet = ['a', 'b']
-    epsilons = ['0']
-    states = [1, 2] 
-    initials = [1] 
-    finals = [2]
+    epsilons = []
+    states = [0,1, 2,3,4,5,6] 
+    initials = [0] 
+    finals = [0,3,4,6,7]
     transitions=[
-    (1,'b',1), (1,'a',1), (1,'c',2), (2,'a',2), (2,'b',2), (2,'c',2)
+    (0,'a',2), (0,'b',1), (1,'b',1), (1,'a',2), (2,'b',2), (2,'a',3),
+    (3,'a',2), (3,'b',6), (7,'b',3), (6,'b',7), (6,'a',5), (5,'a',6),
+    (5,'b',5), (7,'a',5), (4,'a',5), (4,'b',4), (2,'b',2)
     ]
     return automaton.automaton(
     alphabet, epsilons, states, initials, finals, transitions
@@ -17,12 +19,12 @@ def automate():
 def automate2():
        
     alphabet = ['a', 'b', 'c']
-    epsilons = ['0']
+    epsilons = []
     states = ['X', 'Y', 'Z'] 
-    initials = ['X'] 
+    initials = ['X','Z'] 
     finals = ['Z']
     transitions=[
-    ('X','a','X'),('X','c','X'), ('X','b','Y'), ('Y','c','X'), ('Y','b','Y'), ('Y','a','Z'), ('Z','a','Z'), ('Z','c','Z'), ('Z','b','Z')
+    ('X','a','X'),('X','c','X'), ('X','b','Y'), ('Y','c','X'), ('Y','b','Y'), ('Y','a','Z'), ('Z','a','Z'), ('Z','c','Z'), ('Z','b','Z'),('X','a','Y')
     ]
     return automaton.automaton(
     alphabet, epsilons, states, initials, finals, transitions)
@@ -104,18 +106,20 @@ def intersection(auto1, auto2):
     return self
 
 def determinisation(auto):
+    print "appel détérminisation"
     all_states = list()
     fstates = list()
     alphabet = auto.get_alphabet()
     fstates.append(auto.get_initial_states())
-    trasitions = auto.get_transitions()
+    transitions = auto.get_transitions()
+    self = automaton.automaton()
 
-    for in fstates:
+    for s in fstates:
         for al in alphabet:
             for t in transitions:
                 if t[0] in s and al == t[1]:
                     all_states.append(t[2])
-            self.add_transition(s, al, all_states)
+            self.add_transition((automaton.pretty_set(s), al, automaton.pretty_set(all_states)))
             if not all_states in fstates:
                 fstates.append(all_states)
             all_states = list()
@@ -129,19 +133,23 @@ def determinisation(auto):
     for s in self.get_states():
         for f in s:
             if f in auto.get_final_states():
-                if not s in self.get_final_state():
+                if not s in self.get_final_states():
                     self.add_final_state(s)
                     
-    self.add_alphabets(alpha)
+    self.add_character(alphabet)
     initial_states = list()
     initial_states.append(auto.get_initial_states())
     self.add_initial_states(initial_states)
     return self
+
+def minimiser(auto):
+    self = mirror(auto)
+    self = determinisation(self)
+   
     
+    return self
 
 B = automate2( )
 C = automate( )
-self = intersection(B,C)
+self = minimiser(C)
 self.display()
-
-
--- test.py
+++ test.py
@@ -1,18 +1,34 @@
 import automaton
 
+def automate():
+    
+    alphabet = ['a', 'b']
+    epsilons = []
+    states = [0,1, 2,3,4,5,6] 
+    initials = [0] 
+    finals = [0,3,4,6,7]
+    transitions=[
+    (0,'a',2), (0,'b',1), (1,'b',1), (1,'a',2), (2,'b',2), (2,'a',3),
+    (3,'a',2), (3,'b',6), (7,'b',3), (6,'b',7), (6,'a',5), (5,'a',6),
+    (5,'b',5), (7,'a',5), (4,'a',5), (4,'b',4), (2,'b',2)
+    ]
+    return automaton.automaton(
+    alphabet, epsilons, states, initials, finals, transitions
+    )
+
 def automate2():
     
     alphabet = ['a', 'b', 'c']
     epsilons = ['0']
     states = ['X', 'Y', 'Z'] 
-    initials = ['X'] 
+    initials = ['X', 'Z'] 
     finals = ['Z']
     transitions=[
-    ('X','a','X'),('X','c','X'), ('X','b','Y'), ('Y','c','X'), ('Y','b','Y'), ('Y','a','Z'), ('Z','a','Z'), ('Z','c','Z'), ('Z','b','Z')
+    ('X','a','X'),('X','c','X'), ('X','b','Y'), ('Y','c','X'), ('Y','b','Y'), ('Y','a','Z'), ('Z','a','Z'), ('Z','c','Z'), ('Z','b','Z'),('X','a','Y')
     ]
     return automaton.automaton(
     alphabet, epsilons, states, initials, finals, transitions
     )
-B = automate2( )
+B = automate( )
 B.display()
 
